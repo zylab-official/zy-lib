6a3926fd63e85955ec084990b0c364af41560b42
Anton Cherepov <chekitdnb@gmail.com>
2021-07-18 19:07:54 +0200

    fix build

--- projects/zy-lib/src/lib/zy-lib.module.ts
+++ projects/zy-lib/src/lib/zy-lib.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ButtonModule } from './button/button.module';
 import { CheckboxModule } from './checkbox/checkbox.module';
-import { PageTitleComponent } from './page-title/page-title.component';
 import { PageTitleModule } from './page-title/page-title.module';
 import { SplitButtonModule } from './split-button/split-button.module';
 
-const components = [PageTitleComponent]
 
 @NgModule({
-  declarations: components,
   imports: [
     ButtonModule,
     SplitButtonModule,
